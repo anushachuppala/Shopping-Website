4cc9f7ebfe409e9060b749abc182b807d37fa498
anushachuppala <anusha.chuppala12@gmail.com>
2026-06-10 10:18:22 +0530

    added js.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const list = document.querySelector(".navlist");
 const hamburger = document.querySelector(".fa-bars");
 
 hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("fa-x");
+  hamburger.classList.toggle("fa-bars");
+  hamburger.classList.toggle("fa-xmark");
   list.classList.toggle("navlist-active");
 });
